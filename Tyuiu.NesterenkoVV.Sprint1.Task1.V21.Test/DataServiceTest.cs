dc32414e4b1bcbd75fc930b62440d91f2b992658
Вячеслав Нестеренко <Tyuiu.NesterenkoVV@outlook.com>
2025-09-27 13:41:24 +0500

    Task1

---
+++ Tyuiu.NesterenkoVV.Sprint1.Task1.V21.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.NesterenkoVV.Sprint1.Task1.V21.Lib;
+namespace Tyuiu.NesterenkoVV.Sprint1.Task1.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 5.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(2, res);
+        }
+    }
+}
