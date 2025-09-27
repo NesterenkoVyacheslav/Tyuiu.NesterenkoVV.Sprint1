c3c86878fc2e0f1e25a024f6afe7e2950fc955f2
Вячеслав Нестеренко <Tyuiu.NesterenkoVV@outlook.com>
2025-09-27 14:56:12 +0500

    Task2

---
+++ Tyuiu.NesterenkoVV.Sprint1.Task2.V17/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.NesterenkoVV.Sprint1.Task2.V17.Lib;
+namespace Tyuiu.NesterenkoVV.Sprint1.Task2.V17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Нестеренко В. В. | ИИПБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнил: Нестеренко Вячеслав Владимирович | ИИПБ-25-1                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* и переводит минуты в часы                                                ");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+            int x;
+            Console.WriteLine("Введите время в минутах (целое число)");
+            x=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.ConvertMinutesToHours(x) + " целых часов");
+            Console.ReadLine();
+        }
+    }
+}
